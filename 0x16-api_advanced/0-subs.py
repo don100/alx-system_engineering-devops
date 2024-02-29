f39144ad9e1cfafdc486097e7b5b0d230b3e968c
don100 <hajjaj.chiheb1@gmail.com>
2024-02-29 22:48:06 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 '''A module containing functions for working with the Reddit API.
 '''
+
 import requests
+
 
 BASE_URL = 'https://www.reddit.com'
 '''Reddit's base API URL.
 '''
+
 
 def number_of_subscribers(subreddit):
     '''Retrieves the number of subscribers in a given subreddit.
